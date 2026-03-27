2a930532caee77ca880b1194a3542d718f6c7410
gayathrigunda <lakshmigayatrig7878@gmail.com>
2026-03-27 18:02:11 +0530

    Fix PORT

--- customer_support/main.py
+++ customer_support/main.py
@@ -51,6 +51,5 @@ app = main_app
 
 
 if __name__ == "__main__":
-    import uvicorn
     port = int(os.environ.get("PORT", 10000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
